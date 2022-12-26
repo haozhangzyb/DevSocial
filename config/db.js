d544f33c531f0f6a18be3811f4f3eafe913c15b0
Hao Zhang <haozhangzyb@outlook.com>
2022-12-26 16:04:23 +0800

    connect to mongoDB

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(db);
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err);
+    //Exit process with 1
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
