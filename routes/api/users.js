9d6a8e871d4dd33d8c955db7fbf17ccdb334d6d0
Hao Zhang <haozhangzyb@outlook.com>
2022-12-27 12:14:13 +0800

    request and body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+// https://express-validator.github.io/docs/custom-error-messages.html#field-level
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register route
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password with 6 or more characters").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+    console.log("user posting");
+  }
+);
 
+// router.get("/", (req, res) => res.send("user api"));
 module.exports = router;
