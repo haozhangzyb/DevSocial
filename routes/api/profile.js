5fe476b4ccd610624a61118ca1baf7e76e4eb2cf
Hao Zhang <haozhangzyb@outlook.com>
2023-01-05 18:52:17 +0800

    get user’s github repo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,9 @@
-import express, { json } from "express";
+import express from "express";
 const router = express.Router();
 import { check, validationResult } from "express-validator";
 import normalizeURL from "normalize-url";
+import config from "config";
+import axios from "axios";
 
 import auth from "../../middleware/auth.js";
 import Profile from "../../models/Profile.js";
@@ -147,7 +149,7 @@ router.get("/user/:user_id", async (req, res) => {
 router.delete("/", auth, async (req, res) => {
   try {
     await Promise.all([
-      // TODO: remove user posts
+      // @TODO: remove user posts
 
       // Remove profile
       await Profile.findOneAndDelete({ user: req.user.id }),
@@ -276,4 +278,27 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     return res.status(500).json("server error");
   }
 });
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @access  Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubClientSecret")}`,
+    };
+
+    // const gitHubResponse = await axios.get(uri, { headers });
+    const gitHubResponse = await axios.get(uri);
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "No Github profile found" });
+  }
+});
+
 export default router;
