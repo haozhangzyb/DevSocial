72c9954d70901f492325ac0d5fdd6b6df32ea63f
Hao Zhang <haozhangzyb@outlook.com>
2023-01-06 14:46:16 +0800

    add form to register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
-import React, { Fragment } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("password do not match");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <section className='container'>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' onSubmit={submitForm}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            required
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+            required
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Register'
+        />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </section>
+  );
 };
 
 export default Register;
