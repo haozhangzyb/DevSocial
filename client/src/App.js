3cbe0de81cd1f67e1a14c506bf07485041393e6e
Hao Zhang <haozhangzyb@outlook.com>
2023-01-09 21:27:28 +0800

    load user & set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
@@ -11,19 +11,32 @@ import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Alert />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route path='register' element={<Register />} />
-        <Route path='login' element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  // set global header ("x-auth-token")
+  setAuthToken(localStorage.getItem("token"));
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
